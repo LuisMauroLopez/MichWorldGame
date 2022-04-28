12d873b61b9ec5058eb3a3b0f93965c0abe9b337
Wanda Nicole Bravo <86751741+wnicolebravo@users.noreply.github.com>
2022-04-28 10:18:01 -0300

    Update intro.js

--- intro.js
+++ intro.js
@@ -2,10 +2,10 @@
 //Creamos un constructor para armar los personajes de los mich
 
 class MichPlayer {
-    constructor ( nombre, mensaje,sonido){
+    constructor ( nombre, mensaje,){
         this.nombre = nombre;
         this.mensaje = mensaje;
-        this.sonido= sonido;
+        
     }
 
 }
@@ -13,17 +13,38 @@ class MichPlayer {
 const Mich1 = {
     nombre: "Chicho",
     mensaje: "Me gustan los mimitos, me das mimitos?",
-    sonido: sonido1.src = sonidos/mich1.mp3,
 }
 
 const Mich2 = {
     nombre: "Nino",
     mensaje: "Dame pollito humane.",
-    sonido: sonido2.src = sonidos/mich2.mp3,
 }
 
 const Mich3 = {
     nombre: "Gucho",
     mensaje: "HOLA KARENNNNNNNNNN DAME POLLITO POLLITO POLLITO",
-    sonido: sonido3.src = sonidos/mich3.mp3,
 }
+
+let boton1 = document.querySelector("choosebutton1");
+
+    boton1.addEventListener("click", () => {
+      let Mich1sound = document.createElement("mich1")
+      Mich1sound.setAttribute("src", "sonido/mich1.mp3")
+      Mich1sound.play()
+    })
+
+let boton2 = document.querySelector("choosebutton2");
+
+    boton2.addEventListener("click", () => {
+      let Mich2sound = document.createElement("mich2")
+      Mich2sound.setAttribute("src", "sonido/mich2.mp3")
+      Mich2sound.play()
+    })
+
+let boton3 = document.querySelector("choosebutton3");
+
+    boton3.addEventListener("click", () => {
+      let Mich3sound = document.createElement("mich3")
+      Mich3sound.setAttribute("src", "sonido/mich3.mp3")
+      Mich3sound.play()
+    })
