20a41ae353b44194b836ccaf160b0d7e2ff0d7dd
Wanda Nicole Bravo <86751741+wnicolebravo@users.noreply.github.com>
2022-04-28 09:44:31 -0300

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,29 @@
+
+//Creamos un constructor para armar los personajes de los mich
+
+class MichPlayer {
+    constructor ( nombre, mensaje,sonido){
+        this.nombre = nombre;
+        this.mensaje = mensaje;
+        this.sonido= sonido;
+    }
+
+}
+
+const Mich1 = {
+    nombre: "Chicho",
+    mensaje: "Me gustan los mimitos, me das mimitos?",
+    sonido: sonido1.src = sonidos/mich1.mp3,
+}
+
+const Mich2 = {
+    nombre: "Nino",
+    mensaje: "Dame pollito humane.",
+    sonido: sonido2.src = sonidos/mich2.mp3,
+}
+
+const Mich3 = {
+    nombre: "Gucho",
+    mensaje: "HOLA KARENNNNNNNNNN DAME POLLITO POLLITO POLLITO",
+    sonido: sonido3.src = sonidos/mich3.mp3,
+}
